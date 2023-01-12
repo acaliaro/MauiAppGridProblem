05bdb0ab1a416ebcfa570ad20019ab8584760b57
Alessandro Caliaro <alessandro.caliaro@soluzione1.it>
2023-01-12 06:04:38 +0100

    commit source

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiAppGridProblem;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = new MainViewModel();
+	}
+
+	
+}
+
---
+++ MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppGridProblem
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        ObservableCollection<int> valori;
+
+        public MainViewModel()
+        {
+
+            Valori = new ObservableCollection<int>();
+
+            for(int i = 1; i< 100; i++)
+            {
+                Valori.Add(i);
+            }
+        }
+    }
+}
